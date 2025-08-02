1e2ad327509970cd976097c669d82525ca762d1e
yuto-moriizumi <kurvan1112@gmail.com>
2025-08-02 23:47:41 +0900

    色置換フィルタをWebGL向けに実装

--- src/MultiColorReplaceFilter.ts
+++ src/MultiColorReplaceFilter.ts
@@ -1,16 +1,18 @@
 import {
   Color,
   ColorSource,
-  deprecation,
   Filter,
   GlProgram,
   GpuProgram,
+  PointData,
+  Texture,
 } from "pixi.js";
 import { vertex, wgslVertex } from "pixi-filters";
 import fragment from "./multi-color-replace.frag";
 import source from "./multi-color-replace.wgsl";
 
-type DeprecatedColor = number | number[] | Float32Array;
+const COLOR_MAP_WIDTH = 4096;
+const COLOR_MAP_HEIGHT = 4096;
 
 /** Options for the MultiColorReplaceFilter constructor. */
 export interface MultiColorReplaceFilterOptions {
@@ -21,150 +23,77 @@ export interface MultiColorReplaceFilterOptions {
    * _If you wish to change individual elements on the replacement array after instantiation,
    * use the `refresh` function to update the uniforms once you've made the changes_
    */
-  replacements: Array<[ColorSource, ColorSource]>;
-  /**
-   * Tolerance/sensitivity of the floating-point comparison between colors (lower = more exact, higher = more inclusive)
-   * @default 0.05
-   */
-  tolerance?: number;
-  /**
-   * The maximum number of replacements filter is able to use.
-   * Because the fragment is only compiled once, this cannot be changed after construction.
-   * If omitted, the default value is the length of `replacements`
-   */
-  maxColors?: number;
+  replacements: Array<[Color, Color]>;
 }
 
 /**
- * Filter for replacing a color with another color. Similar to ColorReplaceFilter, but support multiple
- * colors.<br>
- * ![original](../screenshots/original.png)![filter](../screenshots/multi-color-replace.png)
- * @class
- * @extends Filter
- *
- * @example
- *  // replaces pure red with pure blue, and replaces pure green with pure white
- *  someSprite.filters = [new MultiColorReplaceFilter({
- *    replacements: [
- *      [0xFF0000, 0x0000FF],
- *      [0x00FF00, 0xFFFFFF]
- *    ],
- *    tolerance: 0.001
- *  })];
- *
- *  You also could use [R, G, B] as the color
- *  someOtherSprite.filters = [new MultiColorReplaceFilter({
- *    replacements: [
- *      [ [1,0,0], [0,0,1] ],
- *      [ [0,1,0], [1,1,1] ]
- *    ],
- *    tolerance: 0.001
- *  })];
- *
+ * Filter for replacing a color with another color.
+ * Similar to pixi-filters/MultiColorReplaceFilter, but faster for large number of replacements.
  */
 export class MultiColorReplaceFilter extends Filter {
-  /** Default values for options. */
-  public static readonly DEFAULT_OPTIONS: MultiColorReplaceFilterOptions = {
-    replacements: [[0xff0000, 0x0000ff]],
-    tolerance: 0.05,
-    maxColors: undefined,
+  private readonly colorMap: Uint8Array;
+  private readonly _replacements: Array<[Color, Color]> = [];
+  public readonly uniforms: {
+    uDisplacementMap: Texture;
   };
-
-  public uniforms: {
-    uOriginalColors: Float32Array;
-    uTargetColors: Float32Array;
-    uTolerance: number;
-  };
-
-  private _replacements: Array<[ColorSource, ColorSource]> = [];
-  private _maxColors: number;
 
   /**
    * @param options - Options for the MultiColorReplaceFilter constructor.
    */
-  constructor(options?: MultiColorReplaceFilterOptions);
-  /**
-   * @deprecated since 6.0.0
-   *
-   * @param {Array<Array>} replacements - The collection of replacement items. Each item is color-pair
-   *        (an array length is 2). In the pair, the first value is original color , the second value
-   *        is target color.
-   * @param {number} [epsilon=0.05] - Tolerance of the floating-point comparison between colors
-   *        (lower = more exact, higher = more inclusive)
-   * @param {number} [maxColors] - The maximum number of replacements filter is able to use. Because the
-   *        fragment is only compiled once, this cannot be changed after construction.
-   *        If omitted, the default value is the length of `replacements`.
-   */
-  constructor(
-    replacements: Array<[DeprecatedColor, DeprecatedColor]>,
-    epsilon?: number,
-    maxColors?: number,
-  );
-  /** @ignore */
-  constructor(
-    ...args:
-      | [MultiColorReplaceFilterOptions?]
-      | [Array<[DeprecatedColor, DeprecatedColor]>, number?, number?]
-  ) {
-    let options = args[0] ?? ({} as MultiColorReplaceFilterOptions);
-
-    if (Array.isArray(options)) {
-      deprecation(
-        "6.0.0",
-        "MultiColorReplaceFilter constructor params are now options object. See params: { replacements, tolerance, maxColors }",
-      );
-
-      options = { replacements: options };
-
-      if (args[1]) options.tolerance = args[1];
-      if (args[2]) options.maxColors = args[2];
-    }
-
-    options = { ...MultiColorReplaceFilter.DEFAULT_OPTIONS, ...options };
-
-    const maxColors = options.maxColors ?? options.replacements.length;
-
+  constructor(options: MultiColorReplaceFilterOptions) {
     const gpuProgram = GpuProgram.from({
       vertex: {
         source: wgslVertex,
         entryPoint: "mainVertex",
       },
       fragment: {
-        source: source.replace(/\$\{MAX_COLORS\}/g, maxColors.toFixed(0)),
+        source: source,
         entryPoint: "mainFragment",
       },
     });
 
     const glProgram = GlProgram.from({
       vertex,
-      fragment: fragment.replace(/\$\{MAX_COLORS\}/g, maxColors.toFixed(0)),
+      fragment: fragment,
       name: "multi-color-replace-filter",
     });
+
+    /**
+     * 置換前の色と置換後の色を格納するテクスチャ。
+     * インデックスは次のようにエンコードされる
+     * R + G*256 + B*256*256
+     * 4096x4096のテクスチャを使用し、各ピクセルが置換後の色を表す。
+     * 大きく16x16のグリッドに分割され、各グリッドは左上から順に各青色に対応する。
+     * たとえば一番左上のグリッドは青色0で、その中で横軸が赤色、縦軸が緑色を表す。
+     * 一番右上のグリッドは青色15で、その中で横軸が赤色、縦軸が緑色を表す。
+     */
+    const length = 4 * COLOR_MAP_WIDTH * COLOR_MAP_HEIGHT;
+    const array = new Uint8Array(length);
+    for (let y = 0; y < COLOR_MAP_HEIGHT; y++) {
+      for (let x = 0; x < COLOR_MAP_WIDTH; x++) {
+        const [r, g, b, a] = pointToIndex({ x, y });
+        array[r] = x % 256;
+        array[g] = y % 256;
+        array[b] = Math.floor(x / 256) + Math.floor(y / 256) * 16;
+        array[a] = 255; // Alpha channel
+      }
+    }
+    // テクスチャのサイズは、GPUの制限に合わせて調整する必要がある。
+    // 手元で試したときは一辺16384(128*128)が最大だった。
+    const texture = getColorMap(array);
 
     super({
       gpuProgram,
       glProgram,
       resources: {
-        multiColorReplaceUniforms: {
-          uOriginalColors: {
-            value: new Float32Array(3 * maxColors),
-            type: "vec3<f32>",
-            size: maxColors,
-          },
-          uTargetColors: {
-            value: new Float32Array(3 * maxColors),
-            type: "vec3<f32>",
-            size: maxColors,
-          },
-          uTolerance: { value: options.tolerance, type: "f32" },
-        },
+        uDisplacementMap: texture.source,
+        uDisplacementSampler: texture.source.style,
       },
     });
-
-    this._maxColors = maxColors;
-
-    this.uniforms = this.resources.multiColorReplaceUniforms.uniforms;
-
+    this.colorMap = array;
+    this.uniforms = {
+      uDisplacementMap: texture,
+    };
     this.replacements = options.replacements;
   }
 
@@ -172,48 +101,18 @@ export class MultiColorReplaceFilter extends Filter {
    * The collection of replacement items. Each item is color-pair
    * (an array length is 2). In the pair, the first value is original color , the second value is target color
    */
-  set replacements(replacements: Array<[ColorSource, ColorSource]>) {
-    const originals = this.uniforms.uOriginalColors;
-    const targets = this.uniforms.uTargetColors;
-    const colorCount = replacements.length;
-    const color = new Color();
+  set replacements(replacements: [Color, Color][]) {
+    replacements.forEach(([source, target]) => {
+      const sourceXY = colorToPoint(source);
+      const sourceIndices = pointToIndex(sourceXY);
+      const [r, g, b] = target.toUint8RgbArray();
+      this.colorMap[sourceIndices[0]] = r;
+      this.colorMap[sourceIndices[1]] = g;
+      this.colorMap[sourceIndices[2]] = b;
+      this.colorMap[sourceIndices[3]] = 255;
+    });
 
-    if (colorCount > this._maxColors) {
-      throw new Error(
-        `Length of replacements (${colorCount}) exceeds the maximum colors length (${this._maxColors})`,
-      );
-    }
-
-    // Fill with negative values
-    originals[colorCount * 3] = -1;
-
-    let r;
-    let g;
-    let b;
-
-    for (let i = 0; i < colorCount; i++) {
-      const pair = replacements[i];
-
-      // for original colors
-      color.setValue(pair[0]);
-
-      [r, g, b] = color.toArray();
-
-      originals[i * 3] = r;
-      originals[i * 3 + 1] = g;
-      originals[i * 3 + 2] = b;
-
-      // for target colors
-      color.setValue(pair[1]);
-
-      [r, g, b] = color.toArray();
-
-      targets[i * 3] = r;
-      targets[i * 3 + 1] = g;
-      targets[i * 3 + 2] = b;
-    }
-
-    this._replacements = replacements;
+    this.uniforms.uDisplacementMap = getColorMap(this.colorMap);
   }
 
   get replacements(): Array<[ColorSource, ColorSource]> {
@@ -228,46 +127,28 @@ export class MultiColorReplaceFilter extends Filter {
   refresh(): void {
     this.replacements = this._replacements;
   }
+}
 
-  /**
-   * The maximum number of color replacements supported by this filter. Can be changed
-   * _only_ during construction.
-   * @readonly
-   */
-  get maxColors(): number {
-    return this._maxColors;
-  }
+/** Convert RGB color to ColorMap coordinates */
+function colorToPoint(color: Color): PointData {
+  const [r, g, b] = color.toUint8RgbArray();
+  const x = r + (b % 16) * 256;
+  const y = g + Math.floor(b / 16) * 256;
+  return { x, y };
+}
 
-  /**
-   * Tolerance of the floating-point comparison between colors (lower = more exact, higher = more inclusive)
-   * @default 0.05
-   */
-  get tolerance(): number {
-    return this.uniforms.uTolerance;
-  }
-  set tolerance(value: number) {
-    this.uniforms.uTolerance = value;
-  }
+/** Convert Point to ColorMap array index */
+function pointToIndex(
+  point: PointData,
+): [r: number, g: number, b: number, a: number] {
+  const r = (point.y * COLOR_MAP_WIDTH + point.x) * 4;
+  return [r, r + 1, r + 2, r + 3];
+}
 
-  /**
-   * @deprecated since 6.0.0
-   *
-   * Tolerance of the floating-point comparison between colors (lower = more exact, higher = more inclusive)
-   * @default 0.05
-   */
-  set epsilon(value: number) {
-    deprecation(
-      "6.0.0",
-      "MultiColorReplaceFilter.epsilon is deprecated, please use MultiColorReplaceFilter.tolerance instead",
-    );
-    this.tolerance = value;
-  }
-  get epsilon(): number {
-    deprecation(
-      "6.0.0",
-      "MultiColorReplaceFilter.epsilon is deprecated, please use MultiColorReplaceFilter.tolerance instead",
-    );
-
-    return this.tolerance;
-  }
+function getColorMap(array: Uint8Array) {
+  return Texture.from({
+    resource: array,
+    width: COLOR_MAP_WIDTH,
+    height: COLOR_MAP_HEIGHT,
+  });
 }
